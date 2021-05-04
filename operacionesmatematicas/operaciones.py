a76a333d8c679619024ec950911834b9eb4b7341
Unknown <unknown@example.com>
2021-05-04 18:07:52 -0500

    cambios

---
+++ operacionesmatematicas/operaciones.py
@@ -0,0 +1,20 @@
+# #Entrada de datos 
+# numero1 = 10
+# numero2 = 15
+# # Calculos y/o  Operaciones
+# suma = numero1 + numero2
+# # Salida de datos 
+# print("Suma = " + str(suma))
+# print(f"Suma = {suma}")
+
+# se debe configurar
+# numero3 =float(input("Escribe un numero: "))
+
+numero3 =int(input("Escribe un numero: "))
+numero4 = input("Escribe otro numero: ")
+
+numero4 = int(numero4)
+
+suma = numero3 + numero4
+
+print("Suma = " + str(suma))
